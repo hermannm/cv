164377cdfcb7bcd11eccd107645a9721385b64d5
hermannm <hermann.morkrid@gmail.com>
2023-11-16 21:11:32 +0100

    change logger initialization

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 )
 
 func main() {
-	logger := slog.New(devlog.NewHandler(os.Stdout, &devlog.Options{Level: slog.LevelDebug}))
-	slog.SetDefault(logger)
+	logHandler := devlog.NewHandler(os.Stdout, &devlog.Options{Level: slog.LevelDebug})
+	slog.SetDefault(slog.New(logHandler))
 
 	flags := parseCommandLineFlags()
 
